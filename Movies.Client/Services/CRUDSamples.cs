2ebf9e729bb4aa1a476a444f08c6777df99e8999
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2025-03-14 22:27:46 -0500

    Switched to Kestrel. Added CRUD code.

--- Movies.Client/Services/CRUDSamples.cs
+++ Movies.Client/Services/CRUDSamples.cs
@@ -1,9 +1,26 @@
 ﻿namespace Movies.Client.Services;
 
-public class CRUDSamples : IIntegrationService
+public class CRUDSamples(IHttpClientFactory httpClientFactory) : IIntegrationService
 {
-    public Task RunAsync()
+    private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+
+    public async Task RunAsync()
     {
-        throw new NotImplementedException();
+        await GetResourceAsync();
+    }
+
+    public async Task GetResourceAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+
+        httpClient.BaseAddress = new Uri("http://localhost:5255");
+        httpClient.Timeout = new TimeSpan(0, 0, 30);
+
+        Thread.Sleep(5000);
+
+        var response = await httpClient.GetAsync("api/movies");
+        response.EnsureSuccessStatusCode();
+
+        var content = response.Content.ReadAsStringAsync();
     }
 }

41557bbefc73b33cf1960259e28a7478e9e35465
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-03-18 17:20:17 -0500

    Added Content Negotiation code.

--- Movies.Client/Services/CRUDSamples.cs
+++ Movies.Client/Services/CRUDSamples.cs
@@ -1,4 +1,9 @@
-﻿namespace Movies.Client.Services;
+﻿using Movies.Client.Models;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Movies.Client.Services;
 
 public class CRUDSamples(IHttpClientFactory httpClientFactory) : IIntegrationService
 {
@@ -11,15 +16,39 @@ public class CRUDSamples(IHttpClientFactory httpClientFactory) : IIntegrationSer
 
     public async Task GetResourceAsync()
     {
-        var httpClient = _httpClientFactory.CreateClient();
-
-        httpClient.BaseAddress = new Uri("http://localhost:5000");
-        httpClient.Timeout = new TimeSpan(0, 0, 30);
+        var httpClient = GetHttpClient();
 
         var response = await httpClient.GetAsync("api/movies");
         response.EnsureSuccessStatusCode();
 
         var content = await response.Content.ReadAsStringAsync();
-        Console.Write(content.ToString());
+
+        var movies = new List<Movie>();
+
+        if (response.Content.Headers.ContentType?.MediaType == "application/json")
+        {
+            movies = JsonSerializer.Deserialize<List<Movie>>(content,
+            new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            });
+        } else if (response.Content.Headers.ContentType?.MediaType == "application/xml")
+        {
+            var serializer = new XmlSerializer(typeof(List<Movie>));
+            movies = serializer.Deserialize(new StringReader(content)) as List<Movie>;
+        }
+    }
+
+    private HttpClient GetHttpClient()
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+
+        httpClient.BaseAddress = new Uri("http://localhost:5000");
+        httpClient.Timeout = new TimeSpan(0, 0, 30);
+        httpClient.DefaultRequestHeaders.Clear();
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+
+        return httpClient;
     }
 }
